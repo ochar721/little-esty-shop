38ed4c0f49cf7dccdb96f56773a1e6d14f5c5c47
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-22 15:59:51 -0600

    Add @holidays to bulk_discount controller and index view

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -2,6 +2,7 @@ class BulkDiscountsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @discounts = @merchant.bulk_discounts
+    @holidays = NationalHolidayService.get_data
   end
 
   def show
