2905b2143232ed248954d913eaaaeec41973e826
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-22 16:02:10 -0600

    Clean up national_holiday_service

--- app/services/national_holiday_service.rb
+++ app/services/national_holiday_service.rb
@@ -8,13 +8,8 @@ class NationalHolidayService
   def self.get_data
     response = conn.get('/Api/v2/NextPublicHolidays/US')
     holidays = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
     holidays[0..2].map do |info|
       NationalHoliday.new(info)
     end
   end
-    # response = Faraday.get("https://date.nager.at/Api/v2/NextPublicHolidays/US")
-    # data = response.body
-    # json = JSON.parse(data, symbolize_names: true)
-    # holidays = json[0..2]
 end
