77db049e8483e1f48b9ea6902c64a82e50b567b5
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-23 17:55:06 -0600

    Add new, create, and private section methods

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -9,4 +9,27 @@ class BulkDiscountsController < ApplicationController
     @merchant = Merchant.find(params[:merchant_id])
     @discount = BulkDiscount.find(params[:id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @bulk_discount = BulkDiscount.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = @merchant.bulk_discounts.create(bulk_discount_params)
+
+    # if bulk_discount.save
+    redirect_to "/merchant/#{@merchant.id}/bulk_discounts"
+    # else
+    #   flash[:error] = "Please fill in all fields. #{error_message(bulk_discount.errors)}."
+    #   redirect_to "/merchant/#{@merchant.id}/bulk_discounts/new"
+    # end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.require(:bulk_discount).permit(:percent_discount, :quantity_threshold, :merchant_id)
+  end
 end
