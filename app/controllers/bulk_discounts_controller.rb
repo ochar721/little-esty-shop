02ae15420e1386eba04734acf9e7dd34914465c0
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-24 12:39:17 -0600

    Add destroy method

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -27,6 +27,12 @@ class BulkDiscountsController < ApplicationController
     # end
   end
 
+  def destroy
+    @merchant = Merchant.find(params[:merchant_id])
+    bulk_discount = BulkDiscount.find(params[:id]).delete
+    redirect_to "/merchant/#{@merchant.id}/bulk_discounts"
+  end
+
   private
 
   def bulk_discount_params
