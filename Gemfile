c3c182870cde7cfa91854f7139e1ac2e3019c687
Klaudia Stewart <klaudiastewart@gmail.com>
2021-04-27 11:08:16 -0600

    Add boxes and images to styles

--- Gemfile
+++ Gemfile
@@ -6,8 +6,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
 ruby '2.5.3'
+gem "gradient"
 gem 'faraday'
 gem 'figaro'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,4 @@
 class ApplicationController < ActionController::Base
-  # def index
-  #   token = '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'
-  #   service = GitHubService.new(token)
-  #   name = service.get_name[:name]
-  # end
 
   private
 
